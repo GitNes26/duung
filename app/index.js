cd506c7204e38d16ef8c99ecc20e90f1bb04685f
GitNes26 <nestorpuentesin@gmail.com>
2022-12-05 23:00:11 -0600

    se corrigio error de audio

--- app/index.js
+++ app/index.js
@@ -2,11 +2,10 @@ import { App } from "./App.js";
 
 
 const audio_theme = document.querySelector("#audio_theme")
-audio_theme.volume = 0.005;
+audio_theme.volume = 0.004;
 
 document.addEventListener("mouseover", () => {
-   if (audio_theme.currentTime == 0)
-      setTimeout(() => audio_theme.play(),1000);
+   audio_theme.muted=false
 })
 
 // audio_theme.play();
