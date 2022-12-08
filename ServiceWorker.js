69291a86ae3be7c65ea5d1306e1d3a6a5c860055
GitNes26 <nestorpuentesin@gmail.com>
2022-12-08 00:07:24 -0600

    se corrigio algun fallo innesperado del Sw.js

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,7 +1,33 @@
-const cacheName = "pwa-duung-v1.0.1";
+const cacheName = "pwa-duung-v1.1.0";
 const filesToCache = [
 	"./",
 	"./index.html",
+	"./favicon.ico",
+
+	"./app/assets/css/loginStyle.css",
+	"./app/assets/css/fondo.css",
+	"./app/assets/css/menu.css",
+	"./app/assets/css/style.css",
+	"./app/views/Splash.js",
+	"./app/views/Login.js",
+	"./app/views/Main.js",
+
+	"./app/assets/fonts/LuckiestGuy-Regular.ttf",
+
+	"./app/assets/plugins/block-ui/jquery.blockui.min.js",
+
+	"./app/assets/audios/musica-espera-separador-musical-.mp3",
+	"./app/assets/audios/correct_answer2.mp3",
+
+	"./app/assets/images/1.png",
+	"./app/assets/images/11.png",
+	"./app/assets/images/logo_duung.png",
+	"./app/assets/images/duung_icon.ico",
+
+	"./app/components/Loader.js",
+
+	"./app/App.js",
+	"./app/index.js",
 
     "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js",
     "https://cdn.jsdelivr.net/npm/sweetalert2@11.1.9/dist/sweetalert2.all.min.js",
@@ -13,40 +39,47 @@ const filesToCache = [
     
 	"./OneSignalSDKWorker.js",
 
-	"./app/assets/css/loginStyle.css",
-	"./app/assets/css/fondo.css",
-	"./app/assets/css/menu.css",
-	"./app/assets/css/style.css",
-
-	"./app/assets/fonts/LuckiestGuy-Regular.ttf",
-
-	"./app/assets/plugins/block-ui/jquery.blockui.min.js",
-
-	"./app/assets/audios/musica-espera-separador-musical-.mp3",
-	"./app/assets/audios/correct_answer2.mp3",
-
-	"./app/assets/images/1.png",
-	"./app/assets/images/logo_duung.png",
-	"./app/assets/images/duung_icon.ico",
 
 	"./app/helpers/api.js",
 	"./app/helpers/fetchRequest.js",
 	"./app/helpers/helpers.js",
 
-	"./app/components/Loader.js",
 	"./app/components/Router.js",
 	"./app/components/SideBar.js",
-
-	// "./app/views/",
-	"./app/views/Splash.js",
-	"./app/views/Login.js",
-	"./app/views/Main.js",
-
-	"./app/App.js",
-	"./app/index.js",
-
-	"./favicon.ico",
 ];
+
+
+// SEGUNDA OPCION
+self.addEventListener("install",installEvent => {
+   installEvent.waitUntil(
+      caches.open(cacheName).then(function(cache) {
+         return cache.addAll(filesToCache);
+      })
+   )
+})
+
+self.addEventListener("fetch",fetchEvent => {
+   fetchEvent.respondWith(
+      caches.match(fetchEvent.request).then( res => {
+         return res || fetch(fetchEvent.request)
+      })
+   )
+})
+
+self.addEventListener("activate", event => {
+   caches.keys().then(keyList => {
+       return Promise.all(
+           keyList.map(key => {
+               if (key !== cacheName) {
+                   console.log("[ServiceWorker] - Removing old cache", key);
+                   return caches.delete(key);
+               }
+           })
+       );
+   });
+   event.waitUntil(self.clients.claim());
+});
+
 
 // "https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback",
 // "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css",
@@ -84,57 +117,6 @@ const filesToCache = [
 //     );
 //  });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// SEGUNDA OPCION
-
-self.addEventListener("install",installEvent => {
-   installEvent.waitUntil(
-      caches.open(cacheName).then(function(cache) {
-         return cache.addAll(filesToCache);
-      })
-   )
-})
-
-self.addEventListener("fetch",fetchEvent => {
-   fetchEvent.respondWith(
-      caches.match(fetchEvent.request).then( res => {
-         return res || fetch(fetchEvent.request)
-      })
-   )
-})
-
-self.addEventListener("activate", event => {
-   caches.keys().then(keyList => {
-       return Promise.all(
-           keyList.map(key => {
-               if (key !== cacheName) {
-                   console.log("[ServiceWorker] - Removing old cache", key);
-                   return caches.delete(key);
-               }
-           })
-       );
-   });
-   event.waitUntil(self.clients.claim());
-});
 
 
 
