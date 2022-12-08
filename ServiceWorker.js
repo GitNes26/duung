11af366ae2c8c2a142ffc7c24ccf507fc025193a
EGPerezR <19170161@uttcampus.edu.mx>
2022-12-08 00:03:39 -0600

    SW

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,51 +1,51 @@
 const cacheName = "pwa-duung-v1.0.1";
 const filesToCache = [
-	"./",
-	"./index.html",
+    "./",
+    "./index.html",
 
-    "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js",
-    "https://cdn.jsdelivr.net/npm/sweetalert2@11.1.9/dist/sweetalert2.all.min.js",
-    "https://cdn.jsdelivr.net/npm/js-cookie@3.0.1/dist/js.cookie.min.js",
-    "https://fonts.googleapis.com/css2?family=Jost:wght@500&display=swap",
-    "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css",
-    "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js",
-    "https://use.fontawesome.com/releases/v5.7.2/css/all.css",
-    
-	"./OneSignalSDKWorker.js",
+    //  "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js",
+    //  "https://cdn.jsdelivr.net/npm/sweetalert2@11.1.9/dist/sweetalert2.all.min.js",
+    //  "https://cdn.jsdelivr.net/npm/js-cookie@3.0.1/dist/js.cookie.min.js",
+    //  "https://fonts.googleapis.com/css2?family=Jost:wght@500&display=swap",
+    //  "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css",
+    //  "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js",
+    //  "https://use.fontawesome.com/releases/v5.7.2/css/all.css",
 
-	"./app/assets/css/loginStyle.css",
-	"./app/assets/css/fondo.css",
-	"./app/assets/css/menu.css",
-	"./app/assets/css/style.css",
+    // "./OneSignalSDKWorker.js",
 
-	"./app/assets/fonts/LuckiestGuy-Regular.ttf",
+    "./app/assets/css/loginStyle.css",
+    "./app/assets/css/fondo.css",
+    "./app/assets/css/menu.css",
+    "./app/assets/css/style.css",
 
-	"./app/assets/plugins/block-ui/jquery.blockui.min.js",
+    "./app/assets/fonts/LuckiestGuy-Regular.ttf",
 
-	"./app/assets/audios/musica-espera-separador-musical-.mp3",
-	"./app/assets/audios/correct_answer2.mp3",
+    "./app/assets/plugins/block-ui/jquery.blockui.min.js",
 
-	"./app/assets/images/1.png",
-	"./app/assets/images/logo_duung.png",
-	"./app/assets/images/duung_icon.ico",
+    "./app/assets/audios/musica-espera-separador-musical-.mp3",
+    "./app/assets/audios/correct_answer2.mp3",
 
-	"./app/helpers/api.js",
-	"./app/helpers/fetchRequest.js",
-	"./app/helpers/helpers.js",
+    "./app/assets/images/1.png",
+    "./app/assets/images/logo_duung.png",
+    "./app/assets/images/duung_icon.ico",
 
-	"./app/components/Loader.js",
-	"./app/components/Router.js",
-	"./app/components/SideBar.js",
+    "./app/helpers/api.js",
+    "./app/helpers/fetchRequest.js",
+    "./app/helpers/helpers.js",
 
-	// "./app/views/",
-	"./app/views/Splash.js",
-	"./app/views/Login.js",
-	"./app/views/Main.js",
+    "./app/components/Loader.js",
+    "./app/components/Router.js",
+    "./app/components/SideBar.js",
 
-	"./app/App.js",
-	"./app/index.js",
+    // "./app/views/",
+    "./app/views/Splash.js",
+    "./app/views/Login.js",
+    "./app/views/Main.js",
 
-	"./favicon.ico",
+    "./app/App.js",
+    "./app/index.js",
+
+    "./favicon.ico",
 ];
 
 // "https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback",
@@ -106,34 +106,34 @@ const filesToCache = [
 
 // SEGUNDA OPCION
 
-self.addEventListener("install",installEvent => {
-   installEvent.waitUntil(
-      caches.open(cacheName).then(function(cache) {
-         return cache.addAll(filesToCache);
-      })
-   )
+self.addEventListener("install", installEvent => {
+    installEvent.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            return cache.addAll(filesToCache);
+        })
+    )
 })
 
-self.addEventListener("fetch",fetchEvent => {
-   fetchEvent.respondWith(
-      caches.match(fetchEvent.request).then( res => {
-         return res || fetch(fetchEvent.request)
-      })
-   )
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+        caches.match(fetchEvent.request).then(res => {
+            return res || fetch(fetchEvent.request)
+        })
+    )
 })
 
 self.addEventListener("activate", event => {
-   caches.keys().then(keyList => {
-       return Promise.all(
-           keyList.map(key => {
-               if (key !== cacheName) {
-                   console.log("[ServiceWorker] - Removing old cache", key);
-                   return caches.delete(key);
-               }
-           })
-       );
-   });
-   event.waitUntil(self.clients.claim());
+    caches.keys().then(keyList => {
+        return Promise.all(
+            keyList.map(key => {
+                if (key !== cacheName) {
+                    console.log("[ServiceWorker] - Removing old cache", key);
+                    return caches.delete(key);
+                }
+            })
+        );
+    });
+    event.waitUntil(self.clients.claim());
 });
 
 
