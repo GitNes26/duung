7c6e10465f58f3c1d578c89f1e53880fbbed60ff
EGPerezR <19170161@uttcampus.edu.mx>
2022-12-07 22:35:53 -0600

    service workers

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,34 +1,32 @@
 const cacheName = "duung-pwa-v1.0.0"
 const filesToCache = [
-   "./",
-   "./index.html",
-   "./app/assets/css/loginStyle.css",
-   "./app/assets/css/fondo.css",
-   "./app/assets/css/menu.css",
-   "./app/assets/css/style.css",
+    "./",
+    "./index.html",
+    "./app/assets/css/loginStyle.css",
+    "./app/assets/css/fondo.css",
+    "./app/assets/css/menu.css",
+    "./app/assets/css/style.css",
 
-   "./app/assets/fonts/LuckiestGuy-Regular.ttf",
+    "./app/assets/fonts/LuckiestGuy-Regular.ttf",
 
-   "./app/assets/images/1.png",
-   "./app/assets/images/logo_duung.png",
-   "./app/assets/images/duung_icon.ico",
+    "./app/assets/images/1.png",
+    "./app/assets/images/logo_duung.png",
+    "./app/assets/images/duung_icon.ico",
 
-   "./app/assets/helpers/",
-   "./app/assets/helpers/api.js",
-   "./app/assets/components/Loader.js",
-   "./app/assets/components/Router.js",
+    "./app/helpers/",
+    "./app/helpers/api.js",
+    "./app/components/Loader.js",
+    "./app/components/Router.js",
 
-   "./app/assets/views/Splash.js",
-   "./app/assets/views/Login.js",
-   "./app/assets/views/Main.js",
+    "./app/views/Splash.js",
+    "./app/views/Login.js",
+    "./app/views/Main.js",
 
-   "./app/assets/App.js",
-   "./app/assets/index.js",
-   
-   "./favicon.ico"
+    "./app/App.js",
+    "./app/index.js",
+
+    "./favicon.ico"
 ];
-
-
 
 // "https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback",
 // "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css",
@@ -40,80 +38,78 @@ const filesToCache = [
 
 
 // Cache on install
-// self.addEventListener("install", e => {
-//    console.log("[ServiceWorker**] - Install");
-//    e.waitUntil(
-//        caches.open(cacheName).then(cache => {
-//            console.log("[ServiceWorker**] - Caching app shell");
-//            return cache.addAll(filesToCache);
-//        })
-//    );
-// });
+self.addEventListener("install", e => {
+    console.log("[ServiceWorker**] - Install");
+    e.waitUntil(
+        caches.open(cacheName).then(cache => {
+            console.log("[ServiceWorker**] - Caching app shell");
+            return cache.addAll(filesToCache);
+        })
+    );
+});
 
-// self.addEventListener("activate", event => {
-//    caches.keys().then(keyList => {
-//        return Promise.all(
-//            keyList.map(key => {
-//                if (key !== cacheName) {
-//                    console.log("[ServiceWorker] - Removing old cache", key);
-//                    return caches.delete(key);
-//                }
-//            })
-//        );
-//    });
-//    event.waitUntil(self.clients.claim());
-// });
+self.addEventListener("activate", event => {
+    caches.keys().then(keyList => {
+        return Promise.all(
+            keyList.map(key => {
+                if (key !== cacheName) {
+                    console.log("[ServiceWorker] - Removing old cache", key);
+                    return caches.delete(key);
+                }
+            })
+        );
+    });
+    event.waitUntil(self.clients.claim());
+});
 
-// self.addEventListener("fetch", event => {
-//    event.respondWith(
-//        caches.match(event.request, { ignoreSearch: true }).then(response => {
-//            return response || fetch(event.request);
-//        })
-//    );
-// });
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request, { ignoreSearch: true }).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
 
 
 
 // Serve from Cache
-// self.addEventListener("fetch", event => {
-//    event.respondWith(
-//       caches.match(event.request)
-//          .then(response => {
-//             return response || fetch(event.request);
-//          })
-//          .catch(() => {
-//             return caches.match('/offline.html');
-//          }
-//       )
-//    )
-// });
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request)
+        .then(response => {
+            return response || fetch(event.request);
+        })
+        .catch(() => {
+            return caches.match('/offline.html');   
+        })   )
+});
 
-self.addEventListener("install",installEvent => {
-   installEvent.waitUntil(
-      caches.open(cacheName).then(function(cache) {
-         return cache.addAll(filesToCache);
-      })
-   )
+self.addEventListener("install", installEvent => {
+    installEvent.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            return cache.addAll(filesToCache);
+        })
+    )
 })
 
-self.addEventListener("fetch",fetchEvent => {
-   fetchEvent.respondWith(
-      caches.match(fetchEvent.request).then( res => {
-         return res || fetch(fetchEvent.request)
-      })
-   )
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+        caches.match(fetchEvent.request).then(res => {
+            return res || fetch(fetchEvent.request)
+        })
+    )
 })
 
 self.addEventListener("activate", event => {
-   caches.keys().then(keyList => {
-       return Promise.all(
-           keyList.map(key => {
-               if (key !== cacheName) {
-                   console.log("[ServiceWorker] - Removing old cache", key);
-                   return caches.delete(key);
-               }
-           })
-       );
-   });
-   event.waitUntil(self.clients.claim());
+    caches.keys().then(keyList => {
+        return Promise.all(
+            keyList.map(key => {
+                if (key !== cacheName) {
+                    console.log("[ServiceWorker] - Removing old cache", key);
+                    return caches.delete(key);
+                }
+            })
+        );
+    });
+    event.waitUntil(self.clients.claim());
 });
