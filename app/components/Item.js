f05431cb51aed72458b350a3d6b69e8e782e4e37
GitNes26 <nestorpuentesin@gmail.com>
2022-12-08 09:28:39 -0600

    Se hizo pull con cesar para vistas de score

--- app/components/Item.js
+++ app/components/Item.js
@@ -1,3 +1,153 @@
-export const Item = (props) => {
-   // let {q}
+export const Item = (props={}) => {
+   // let answers, id, question
+   // if (props) {answers, id, question} = props;
+   let {answers, id, question} = props;
+   return`
+      <div
+         class="card__BlocGame position-absolute start-50 translate-middle-x"
+      >
+         <div class="row">
+            <center
+               class="col-md-12"
+               style="
+                  background-color: #ffffff;
+                  border-radius: 20px;
+                  height: 380px;
+                  padding: 100px;
+               "
+            >
+               <h1 class="fw-light text1__game">¿La pregunta es?</h1>
+            </center>
+         </div>
+         <br />
+         <div class="row">
+            <center
+               class="col-md-12 push"
+               type="button"
+               style="
+                  background-color: #ffffff;
+                  border-radius: 50px;
+                  height: 80px;
+                  margin-top: 10px;
+               "
+            >
+               <div style="padding: 20px" class="row text-start">
+                  <div
+                     class="col-md-2 col-2"
+                     style="
+                        background-color: #dc0084;
+                        border-radius: 50%;
+                        font-weight: bold;
+                        width: 40px;
+                        height: 40px;
+                        padding: 5px;
+                        padding-left: 15px;
+                        color: #ffffff;
+                     "
+                  >
+                     a)
+                  </div>
+                  <h4 class="fw-light col-md-10 col-10 text2__game">
+                     Respuesta a
+                  </h4>
+               </div>
+            </center>
+
+            <center
+               class="col-md-12 push"
+               type="button"
+               style="
+                  background-color: #ffffff;
+                  border-radius: 50px;
+                  height: 80px;
+                  margin-top: 10px;
+               "
+            >
+               <div style="padding: 20px" class="row text-start">
+                  <div
+                     class="col-md-2 col-2"
+                     style="
+                        background-color: #0088dc;
+                        border-radius: 50%;
+                        font-weight: bold;
+                        width: 40px;
+                        height: 40px;
+                        padding: 5px;
+                        padding-left: 15px;
+                        color: #ffffff;
+                     "
+                  >
+                     b)
+                  </div>
+                  <h4 class="fw-light col-md-10 col-10 text2__game">
+                  Respuesta b
+                  </h4>
+               </div>
+            </center>
+
+            <center
+               class="col-md-12 push"
+               type="button"
+               style="
+                  background-color: #ffffff;
+                  border-radius: 50px;
+                  height: 80px;
+                  margin-top: 10px;
+               "
+            >
+               <div style="padding: 20px" class="row text-start">
+                  <div
+                     class="col-md-2 col-2"
+                     style="
+                        background-color: #1adc00;
+                        border-radius: 50%;
+                        font-weight: bold;
+                        width: 40px;
+                        height: 40px;
+                        padding: 5px;
+                        padding-left: 15px;
+                        color: #ffffff;
+                     "
+                  >
+                     c)
+                  </div>
+                  <h4 class="fw-light col-md-10 col-10 text2__game">
+                  Respuesta c
+                  </h4>
+               </div>
+            </center>
+            <center
+               class="col-md-12 push"
+               type="button"
+               style="
+                  background-color: #ffffff;
+                  border-radius: 50px;
+                  height: 80px;
+                  margin-top: 10px;
+               "
+            >
+               <div style="padding: 20px" class="row text-start">
+                  <div
+                     class="col-md-2 col-2"
+                     style="
+                        background-color: #c47600;
+                        border-radius: 50%;
+                        font-weight: bold;
+                        width: 40px;
+                        height: 40px;
+                        padding: 5px;
+                        padding-left: 15px;
+                        color: #ffffff;
+                     "
+                  >
+                     d)
+                  </div>
+                  <h4 class="fw-light col-md-10 col-10 text2__game">
+                  Respuesta d
+                  </h4>
+               </div>
+            </center>
+         </div>
+      </div>
+   `
 }
--- app/views/Game.js
+++ app/views/Game.js
@@ -1,3 +1,4 @@
+import { Item } from "../components/Item.js";
 import { SideBar } from "../components/SideBar.js";
 import { getCookie, route } from "../helpers/helpers.js";
 
@@ -34,6 +35,8 @@ export function Game() {
 
    $fragment.appendChild(SideBar());
 
+   
+
    $context.classList.add("context");
 	$context.innerHTML = `
       <div class="shadow reloj__position">
@@ -41,119 +44,11 @@ export function Game() {
       </div>
 
       <main class="bloquePrincipal">
-         <div class="position-relative">
-            <div
-               class="card__BlocGame position-absolute start-50 translate-middle-x"
-            >
-               <div class="row">
-                  <center
-                     class="col-md-12"
-                     style="
-                        background-color: #ffffff;
-                        border-radius: 20px;
-                        height: 380px;
-                        padding: 100px;
-                     "
-                  >
-                     <h1 class="fw-light text1__game">¿La pregunta es?</h1>
-                  </center>
-               </div>
-               <br />
-               <div class="row">
-                  <center
-                     class="col-md-12 push"
-                     type="button"
-                     style="
-                        background-color: #ffffff;
-                        border-radius: 50px;
-                        height: 80px;
-                        margin-top: 10px;
-                     "
-                  >
-                     <div style="padding: 20px" class="row text-start">
-                        <div
-                           class="col-md-2 col-2"
-                           style="
-                              background-color: #dc0084;
-                              border-radius: 50%;
-                              width: 40px;
-                              height: 40px;
-                              padding: 5px;
-                              padding-left: 15px;
-                              color: #ffffff;
-                           "
-                        >
-                           a)
-                        </div>
-                        <h4 class="fw-light col-md-10 col-10 text2__game">
-                           ¿La pregunta es?
-                        </h4>
-                     </div>
-                  </center>
+         <div class="position-relative">`;
 
-                  <center
-                     class="col-md-12 push"
-                     type="button"
-                     style="
-                        background-color: #ffffff;
-                        border-radius: 50px;
-                        height: 80px;
-                        margin-top: 10px;
-                     "
-                  >
-                     <div style="padding: 20px" class="row text-start">
-                        <div
-                           class="col-md-2 col-2"
-                           style="
-                              background-color: #0088dc;
-                              border-radius: 50%;
-                              width: 40px;
-                              height: 40px;
-                              padding: 5px;
-                              padding-left: 15px;
-                              color: #ffffff;
-                           "
-                        >
-                           b)
-                        </div>
-                        <h4 class="fw-light col-md-10 col-10">
-                           ¿La pregunta es?
-                        </h4>
-                     </div>
-                  </center>
-
-                  <center
-                     class="col-md-12 push"
-                     type="button"
-                     style="
-                        background-color: #ffffff;
-                        border-radius: 50px;
-                        height: 80px;
-                        margin-top: 10px;
-                     "
-                  >
-                     <div style="padding: 20px" class="row text-start">
-                        <div
-                           class="col-md-2 col-2"
-                           style="
-                              background-color: #1adc00;
-                              border-radius: 50%;
-                              width: 40px;
-                              height: 40px;
-                              padding: 5px;
-                              padding-left: 15px;
-                              color: #ffffff;
-                           "
-                        >
-                           c)
-                        </div>
-                        <h4 class="fw-light col-md-10 col-10">
-                           ¿La pregunta es?
-                        </h4>
-                     </div>
-                  </center>
-               </div>
-            </div>
+      $container.innerHTML += Item()
+         
+      $container.innerHTML +=`
          </div>
          <img
             class="d-md-block d-none position-absolute start-50 translate-middle-x img__central1"
